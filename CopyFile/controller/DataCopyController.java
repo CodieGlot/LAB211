caf57c96ccd6ad43a83dfdbc9c649c1c681097b7
CodieGlot <ngnhathuy1224@gmail.com>
2023-11-01 11:08:24 +0700

    Add Search and Copy File

---
+++ CopyFile/controller/DataCopyController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.FileData;
+import repository.DataCopyRepository;
+import repository.IDataCopyRepository;
+
+/**
+ *
+ * @author Admin
+ */
+public class DataCopyController {
+
+    protected IDataCopyRepository repo;
+    protected FileData data;
+
+    public DataCopyController() {
+        repo = new DataCopyRepository();
+        data = new FileData();
+    }
+
+    public void run() {
+        repo.dataCopy(data);
+    }
+
+}
